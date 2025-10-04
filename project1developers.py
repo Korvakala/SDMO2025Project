eecbb64214c0c3e6f25196ba9715c1fb872bce2d
mibska <sintti.b@gmail.com>
2025-10-04 10:39:47 +0300

    Update project1developers.py
    Muutettu koodia siten, että ehtoja täytyy olla 2, jotta vertailu aloitetaan.

--- project1developers.py
+++ project1developers.py
@@ -120,7 +120,8 @@ df["c1_check"] = df["c1"] >= t
 df["c2_check"] = df["c2"] >= t
 df["c3_check"] = (df["c3.1"] >= t) & (df["c3.2"] >= t)
 # Keep only rows where at least one condition is True
-df = df[df[["c1_check", "c2_check", "c3_check", "c4", "c5", "c6", "c7"]].any(axis=1)]
+checks = ["c1_check", "c2_check", "c3_check", "c4", "c5", "c6", "c7"]
+df = df[df[checks].sum(axis=1) >= 2]
 
 # Omit "check" columns, save to csv
 df = df[["name_1", "email_1", "name_2", "email_2", "c1", "c2",
