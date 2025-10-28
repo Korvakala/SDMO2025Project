586964e351e85d7e6c6e0ad668da27416965a658
mibska <sintti.b@gmail.com>
2025-10-28 13:42:53 +0200

    Update project1developers.py
    t = 0.8 --> t = 0.6

--- project1developers.py
+++ project1developers.py
@@ -114,7 +114,7 @@ df.to_csv(os.path.join("project1devs", "devs_similarity.csv"), index=False, head
 
 
 # Set similarity threshold, check c1-c3 against the threshold
-t = 0.8
+t = 0.6
 df["c1_check"] = df["c1"] >= t
 df["c2_check"] = df["c2"] >= t
 df["c3_check"] = (df["c3.1"] >= t) & (df["c3.2"] >= t)
