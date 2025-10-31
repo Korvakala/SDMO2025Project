eb80ed977a5d02db3dbaf030fd5fe3fa08d076f4
Korvakala <116496745+Korvakala@users.noreply.github.com>
2025-10-31 16:50:49 +0200

    wrote tests

--- tests/test_project1developers.py
+++ tests/test_project1developers.py
@@ -1,3 +1,32 @@
 import pytest
-from project1developers import 
+from project1developers import *
 
+def test_extract_devs():
+    # Implement this!
+
+    # assert extract_devs() == 
+
+    pass
+
+def test_read_devs():
+    # Implement this!
+    pass
+
+def test_compute_similarity():
+    # Implement this!
+    pass
+
+def test_preprocess():
+    # Implement this!
+    pass
+
+def test_save_similarity_data():
+    # Implement this!
+    pass
+
+def filter_and_save():
+    # Implement this!
+    pass
+
+
+# def test_main(): 
