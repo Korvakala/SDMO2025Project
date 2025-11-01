c9145e6b18c86702538b5cd815c00c78984ce1b7
Korvakala <116496745+Korvakala@users.noreply.github.com>
2025-11-01 09:44:06 +0200

    Unit test

--- tests/test_project1developers.py
+++ tests/test_project1developers.py
@@ -1,13 +1,6 @@
 import pytest
 from project1developers import *
 
-def test_extract_devs():
-    # Implement this!
-
-    # assert extract_devs() == 
-
-    pass
-
 def test_read_devs():
     # Implement this!
     pass
@@ -17,8 +10,20 @@ def test_compute_similarity():
     pass
 
 def test_preprocess():
-    # Implement this!
-    pass
+    # useampia kuin kaksi osaisia nimiä (biggus dickus jr -> biggus, dickus jr)
+    # Yksiosainen nimi
+    # aksenttimerkkejä
+    #
+    dev = [" Biggus. Dickus!", "biggus.dickus@monty.com"]
+    name, first, last, i_first, i_last, email, prefix = preprocess(dev)
+    assert name == "biggus dickus"
+    assert first == "biggus"
+    assert last == "dickus"
+    assert i_first == "b"
+    assert i_last == "d"
+    assert email == "biggus.dickus@monty.com"
+    assert prefix == "biggus.dickus"
+
 
 def test_save_similarity_data():
     # Implement this!
@@ -28,5 +33,4 @@ def filter_and_save():
     # Implement this!
     pass
 
-
-# def test_main(): 
+ 
